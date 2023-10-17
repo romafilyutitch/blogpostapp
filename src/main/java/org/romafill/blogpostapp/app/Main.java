efb68688307b1cf9eea0e4419e7151c62ad21228
Roman Filiyutich <roman.filiyutich@gmail.com>
2023-10-17 22:29:22 +0300

    Use spring and LocalSessionFactoryBean

--- src/main/java/org/romafill/blogpostapp/app/Main.java
+++ src/main/java/org/romafill/blogpostapp/app/Main.java
@@ -3,55 +3,34 @@ package org.romafill.blogpostapp.app;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
-import org.romafill.blogpostapp.entity.Comment;
 import org.romafill.blogpostapp.entity.Post;
-import org.romafill.blogpostapp.entity.User;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Configuration configure = new Configuration().configure("hibernate.cfg.xml");
 
-        SessionFactory sessionFactory = configure.buildSessionFactory();
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        SessionFactory sessionFactory = applicationContext.getBean(SessionFactory.class);
 
         Transaction transaction = null;
 
         try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
 
-            Query<User> userQuery = session.createQuery("from User ", User.class);
-            List<User> users = userQuery.list();
-            User user = null;
-            if (users.isEmpty()) {
-                user = new User();
-                user.setUsername("Roma fill");
-                session.persist(user);
-            } else {
-                user = users.get(0);
+            Query<Post> postsQuery = session.createQuery("from Post", Post.class);
+            List<Post> posts = postsQuery.list();
+
+
+            for (Post post : posts) {
+                System.out.println(post);
             }
 
-            Post post = new Post();
-            post.setTitle("Test post");
-            post.setText("Test post to check the comments");
-            post.setAuthor(user);
-
-            Comment comment = new Comment();
-            comment.setAuthor(user);
-            comment.setText("Test comment for post");
-
-            post.getComments().add(comment);
-
-            session.persist(post);
             transaction.commit();
 
-            Query<Comment> fromComment = session.createQuery("from Comment ", Comment.class);
-            List<Comment> list = fromComment.list();
-            for (Comment comment1 : list) {
-                System.out.println(comment1);
-            }
 
         } catch (Exception e) {
             e.printStackTrace();
