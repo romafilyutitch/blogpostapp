c52eaefee6c3d1a490917879a45c35722c09de94
romafilyutitch <romafilyutitch@yandex.by>
2023-10-18 21:39:42 +0300

    Test github user

--- src/main/java/org/romafill/blogpostapp/entity/Comment.java
+++ src/main/java/org/romafill/blogpostapp/entity/Comment.java
@@ -24,7 +24,7 @@ public class Comment {
     }
 
     public User getAuthor() {
-        System.out.println("Test git hub12312345413424dsfsdfsdff");
+        System.out.println("Test git hub1sdfsdfsdfsdfsfgsdfgdfgf2312345413424sdsdafsdffsdfdsfsdfsdff");
         return author;
     }
 
