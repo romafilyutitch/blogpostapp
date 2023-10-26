fd0c8aeb0a2d90ed6396468933540fb1d4c44775
romafilyutitch <romafilyutitch@yandex.by>
2023-10-26 22:21:19 +0300

    change context root to /blogpostapp

--- src/main/java/org/romafill/blogpostapp/controller/HelloController.java
+++ src/main/java/org/romafill/blogpostapp/controller/HelloController.java
@@ -1,11 +1,20 @@
 package org.romafill.blogpostapp.controller;
 
+import org.romafill.blogpostapp.service.IPostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MyController {
+public class HelloController {
+
+    private final IPostService postService;
+
+    public HelloController(IPostService postService) {
+        this.postService = postService;
+    }
+
     @GetMapping("/hello")
     public String hello(Model model) {
         System.out.println("hello");
